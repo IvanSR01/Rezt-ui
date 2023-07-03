f1d94ba4efabb686202264bad207d5e7af8d2a76
Ivan Nulson <ivan.sryvkin@bk.ru>
2023-07-03 11:04:59 +0300

    Button update

--- src/lib/components/Button/Button.tsx
+++ src/lib/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import { FC, MouseEvent, ReactNode } from 'react'
 import styles from './Button.module.css'
 import clsx from 'clsx'
 import { ClassesButton } from './ClassesButton'
@@ -6,17 +6,36 @@ interface IButton {
 	children: ReactNode
 	variant?: string
 	color?: string
+	onClick?: (value?: any) => void
+	disabled?: boolean
+	className?: string
 }
 
-const Button: FC<IButton> = ({ children, variant, color }) => {
-	const classes: string = (variant ? variant : 'contained') + (color ? color : 'primary')
-	console.log(classes)
-	
+const Button: FC<IButton> = ({
+	children,
+	variant,
+	color,
+	disabled,
+	onClick,
+	className = ''
+}) => {
+	const classes: string =
+		(variant ? variant : 'contained') + (color ? color : 'primary')
+
+	const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+		if (disabled || !onClick) return
+		onClick && onClick(e)
+	}
+
 	if (!children) return
 	const textButton = children.toString().toUpperCase()
 	return (
 		<div>
-			<button className={clsx(styles.button, ClassesButton[classes])}>
+			<button
+				disabled={disabled}
+				className={clsx(styles.button, ClassesButton[classes], className)}
+				onClick={handleClick}
+			>
 				{textButton}
 			</button>
 		</div>
